5496cc8b345fc8428c6a8dc1c29e1018ae73c630
janripken <janripken@protonmail.com>
2024-06-27 09:48:53 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,46 @@
           <p>{{ info.details }}</p>
         </div>
         <button @click="scrollTo('section1')" class="scroll-button">Zurück nach oben ↑</button>
+        <button @click="scrollTo('section3')" class="scroll-button">Zeitleiste ansehen ↓</button>
       </div>
     </section>
+
+    <section id="section3" class="background-section" :style="section3Style">
+      <div class="content">
+        <h2>Programmablauf</h2>
+        <ul class="timeline">
+          <li v-for="(event, index) in timeline" :key="index" class="timeline-item">
+            <h3>{{ event.time }}</h3>
+            <p>{{ event.activity }}</p>
+          </li>
+        </ul>
+        <button @click="scrollTo('section2')" class="scroll-button">Zurück nach oben ↑</button>
+        <button @click="scrollTo('section4')" class="scroll-button">Anreiseinformationen ↓</button>
+      </div>
+    </section>
+
+    <section id="section4" class="background-section" :style="section4Style">
+      <div class="content">
+        <h2>Ort & Anreise</h2>
+        <div class="map-container">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d602.282562854184!2d8.110818534503748!3d53.517629804975385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b6930947ae5291%3A0x9d72643092521aa7!2sMoor%20Beauty!5e1!3m2!1sde!2sde!4v1719472841141!5m2!1sde!2sde" width="700" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <button @click="scrollTo('section3')" class="scroll-button">Zurück zur Zeitleiste ↑</button>
+        <button @click="scrollTo('section5')" class="scroll-button">Zur Umfrage ↓</button>
+      </div>
+    </section>
+
+
+    <section id="section5" class="background-section" :style="section5Style">
+      <div class="content">
+        <h2>Teilnahmebestätigung</h2>
+        <div class="form-container">
+         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd1vwXNNb1fHw3AMecxyLP4QKFvARw69hBAd8Z8jOQDBiCOxQ/viewform?embedded=true" width="640" height="466" frameborder="0" marginheight="0" marginwidth="0">Wird geladen…</iframe>
+        </div>
+        <button @click="scrollTo('section4')" class="scroll-button">Zurück zur Karte ↑</button>
+      </div>
+    </section>
+
   </div>
 </template>
 
@@ -28,11 +66,16 @@ export default {
   data() {
     return {
       partyInfo: [
-        { title: 'Datum & Zeit', details: '5. Juli 2024, 12:00 Uhr - Open End' },
+        { title: 'Datum & Zeit', details: '5. Juli 2024, 13:00 Uhr - Open End' },
         { title: 'Dresscode', details: 'Lässige Sommerkleidung' },
-        { title: 'Aktivitäten', details: 'Alkohol, Koks und Nutten' },
+        { title: 'Aktivitäten', details: 'Besuch des Wochenende an der Jade' },
         { title: 'Der Plan', details: 'Vortrinken und Afterparty bei uns' },
-        { title: 'Essen & Getränke', details: 'Bier, Bier und Bier ' },
+        { title: 'Info', details: 'Wir stellen nur Bier, Charly und ein paar Kleinigkeiten. Solltet ihr etwas anderes wollen, bringt es bitte selbst mit.' },
+      ],
+      timeline: [
+        { time: '13:00 Uhr', activity: 'Treffen bei uns' },
+        { time: '16:00 Uhr', activity: 'Besuch des Wocheende an der Jade' },
+        { time: 'ab 21:00 Uhr', activity: 'Afterparty' },
       ],
     };
   },
@@ -52,7 +95,31 @@ export default {
         'background-position': 'top',
         'height': '100vh',
       };
-    }
+    },
+    section3Style() {
+      return {
+        'background-image': `url(${require('@/assets/hafen.jpeg')})`,
+        'background-size': 'cover',
+        'background-position': 'center',
+        'height': '100vh',
+      };
+    },
+    section4Style() {
+      return {
+        'background-image': `url(${require('@/assets/schiffe.jpeg')})`,
+        'background-size': 'cover',
+        'background-position': 'center',
+        'height': '100vh',
+      };
+    },
+section5Style() {
+      return {
+        'background-image': `url(${require('@/assets/ende.jpeg')})`,
+        'background-size': 'cover',
+        'background-position': 'center',
+        'height': '100vh',
+      };
+    },
   },
   methods: {
     scrollTo(sectionId) {
@@ -106,6 +173,7 @@ header h1 {
 
 .content {
   max-width: 600px;
+  width: 100vh;
   padding: 20px;
   background-color: rgba(0, 0, 0, 0.5);
   border-radius: 10px;
@@ -126,6 +194,7 @@ header h1 {
   padding: 10px 20px;
   color: white;
   font-size: 16px;
+  margin: 5px;
   cursor: pointer;
   border-radius: 5px;
   transition: background-color 0.3s;
@@ -133,6 +202,41 @@ header h1 {
 
 .scroll-button:hover {
   background-color: #00796b;
+}
+
+.timeline {
+  list-style: none;
+  padding: 0;
+  
+}
+
+.timeline-item {
+  background-color: rgba(0, 150, 136, 0.8);
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+
+.map-container {
+  width: 100%;
+  height: 450px;
+  margin-bottom: 20px;
+}
+
+.form-container {
+  width: 100%;
+  height: calc(50vh ); /* subtract header height if applicable */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+
+}
+
+iframe {
+  width: 100%;
+  height: 100%;
+  border: 0;
 }
 
 /* Media Queries for responsive design */
@@ -151,6 +255,10 @@ header h1 {
   .scroll-button {
     padding: 8px 16px;
     font-size: 14px;
+  }
+
+  .map-container {
+    height: 300px;
   }
 }
 </style>
